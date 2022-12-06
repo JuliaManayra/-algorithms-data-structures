b6f1e9974da63b96c803e92d92effe71ca4da6a2
ed99a5ed e8cc512c059a <8ec5a2d0.8aed.4df4.8e50.8a83ff@gmail.com>
2022-12-06 11:10:30 -0300

    queue (Fifo) on vector integer and Arralist<Integer>

---
+++ queue/queue/src/main/java/org/data/struture/MainQueueVector.java
@@ -0,0 +1,30 @@
+package org.data.struture;
+
+public class MainQueueVector {
+    public static void main(String[] args) {
+
+
+        QueueVector queueVector = new QueueVector(10);
+
+        queueVector.setValueOnQueue(1);
+        queueVector.setValueOnQueue(2);
+        queueVector.setValueOnQueue(3);
+        queueVector.setValueOnQueue(4);
+        queueVector.printAllElementsOfQueue();
+        System.out.println("Remove 2 elements");
+        queueVector.getValueOnQueue();
+        queueVector.getValueOnQueue();
+        queueVector.printAllElementsOfQueue();
+        System.out.println("Add 2 elements");
+        queueVector.setValueOnQueue(37);
+        queueVector.setValueOnQueue(10);
+        queueVector.printAllElementsOfQueue();
+
+        System.out.println("Add 10 elements");
+        queueVector.setValueOnQueue(55);
+        queueVector.setValueOnQueue(67);
+        queueVector.setValueOnQueue(90);
+        queueVector.setValueOnQueue(100);
+        queueVector.printAllElementsOfQueue();
+    }
+}
